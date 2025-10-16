8ce092433aa62b575ba6a9b7628bbf41000cc5b8
skyoon <dzn.skyoon@gmail.com>
2025-10-17 08:50:22 +0900

    exercise

---
+++ linux_c/exercise.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Be happy\n");
+    printf("My friend\n");
+    printf("MY\tfriend\n");
+    printf("Goot\bd\tchance\n");
+    printf("Cow\rW\a\n");
+    printf("abcd\r12\t34\n");
+
+    
+    return 0;
+
+}
+
+
