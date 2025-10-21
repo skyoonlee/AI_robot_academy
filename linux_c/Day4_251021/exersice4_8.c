af3d43af11f11bd00354b2a15c8e9334e1f22ce4
Skyoonlee <dzn.skyoon@gmail.com>
2025-10-21 16:51:21 +0900

    exersice4

---
+++ linux_c/Day4_251021/exersice4_8.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main(void) {
+
+double data[36];
+int i, min_index1, min_index2, min_index3 = 0;
+double min_value1, min_value2, min_value3;
+
+srand(time(NULL));
+
+for (i = 0; i <= 36; i++){
+
+    data[i] = rand() % 500;
+     
+ }
+
+min_value1 = data[0];
+min_value2 = data[0];
+min_value3 = data[0];
+
+for (i = 1; i < 36; i++){
+    if (data[i] < min_value1){
+        min_value1 = data[i];
+        min_index1 = i;
+        }
+    }
+
+
+
+
+
+
+printf("\n[라이다 가장 가까운 탐색 결과 (3개)]\n");
+printf("가장 가까운 거리: %.2f cm\n", min_value1, min_value2, min_value3);
+printf("장애물 방향: %d 도\n", min_index1, min_index2, min_index3);
+
+    return 0;
+}
