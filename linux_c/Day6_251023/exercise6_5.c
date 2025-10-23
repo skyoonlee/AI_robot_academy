ee35f0caf406a132385bbbadf21752fef3619b0e
Skyoonlee <dzn.skyoon@gmail.com>
2025-10-23 16:51:46 +0900

    exersice

--- linux_c/Day6_251023/exercise6_5.c
+++ linux_c/Day6_251023/exercise6_5.c
@@ -6,7 +6,6 @@ int a;
 a = 10;
 
 int *pa;
-
 pa = &a;
 
 printf ("포인터 pa에 변수 a 대입 결과값: %d\n", *pa);
@@ -14,6 +13,7 @@ printf ("포인터 pa에 변수 a 대입 결과값: %d\n", *pa);
 *pa = 20;
 
 printf ("포인터 pa에 간접 참조 연산자 대입 결과값 : %d\n", *pa);
+printf ("변경 후 a값 : %d\n", a);
 
 return 0;
 }
